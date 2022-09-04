70e10e36732b6007dd9eae24015c9b9556820b35
akashp69 <akashpakhare499@gmail.com>
2022-09-05 00:05:10 +0530

    Added: Code for User need to enter valid Name

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,2 +1,13 @@
+
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    public static boolean isValidUserName(String firstName) {
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-zA-z0-9]{2,}$");
+        return pattern.matcher(firstName).matches();
+    }
+    public static void main(String[] args) {
+        String str1 = "Akash";
+        System.out.println(isValidUserName(str1));
+    }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -1,2 +1,9 @@
-public class UserRegistrationTest {
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+class UserRegistrationTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assertions.assertTrue(true);
+    }
 }
