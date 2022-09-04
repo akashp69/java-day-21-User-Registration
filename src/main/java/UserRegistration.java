dda9b94d539a43b5f95cfdf7cbc1ec1c4aaad6b1
akashp69 <akashpakhare499@gmail.com>
2022-09-05 00:57:56 +0530

    Added: Code for User need to enter  valid Password rule4

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,14 +1,19 @@
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-
+/*	  create parameterised method name as isValidFirstname
+	  In this method function to validate the first name
+	  @param first name as string
+	  @return first name*/
 
     public static boolean isValidFirstName(String firstName) {
         Pattern pattern = Pattern.compile("^[A-Z][a-zA-z0-9]{2,}$");
-
+/*		Regex to check valid username and compile the regex
+		Pattern class contains matcher() method to find matching between given username
+		and regular expression.*/
 
         return pattern.matcher(firstName).matches();
-
+//		Return if the firstname matched the Regex
     }
     public static boolean isValidLastName(String lastName) {
         Pattern pattern = Pattern.compile("^[A-Z][a-zA-z0-9]{2,}$");
@@ -27,14 +32,21 @@ public class UserRegistration {
         return pattern.matcher(password1).matches();
     }
     public static boolean isValidPassword2(String password2) {
-        Pattern pattern = Pattern.compile("^[A-Z][a-zA-z0-9]{8,}$");
+        Pattern pattern = Pattern.compile("^[A-Z][a-zA-z0-9]{7,}$");
         return pattern.matcher(password2).matches();
     }
     public static boolean isValidPassword3(String password3) {
         Pattern pattern = Pattern.compile("^[A-Z][a-zA-z1-9]{6,}[1-9]$");
         return pattern.matcher(password3).matches();
     }
-
+    public static boolean isValidPassword4(String password4) {
+        Pattern pattern = Pattern.compile("^[A-Z][a-zA-z1-9]{5,}[@$^][1-9]$");
+        return pattern.matcher(password4).matches();
+    }
+    /*
+     * create a main method , all program execute in main method
+     * @param args no arguments
+     */
     public static void main(String[] args) {
         System.out.println(isValidFirstName("Akash"));
         System.out.println(isValidLastName("Pakhare"));
@@ -43,5 +55,6 @@ public class UserRegistration {
         System.out.println(isValidPassword1("akash499"));
         System.out.println(isValidPassword2("Akash499"));
         System.out.println(isValidPassword3("Akashpakhare4"));
+        System.out.println(isValidPassword4("Akash$4"));
     }
 }
