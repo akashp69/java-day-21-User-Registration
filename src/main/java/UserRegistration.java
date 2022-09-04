c0422d97800f066da7a8fa24350e6870b11709ba
akashp69 <akashpakhare499@gmail.com>
2022-09-05 00:53:13 +0530

    Added: Code for User need to enter  valid Password rule3

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -10,32 +10,30 @@ public class UserRegistration {
         return pattern.matcher(firstName).matches();
 
     }
-
     public static boolean isValidLastName(String lastName) {
         Pattern pattern = Pattern.compile("^[A-Z][a-zA-z0-9]{2,}$");
         return pattern.matcher(lastName).matches();
     }
-
     public static boolean isValidPhoneNo(String phoneNo) {
         Pattern pattern = Pattern.compile("^[0-9]{1,2} \\d{10}$");
         return pattern.matcher(phoneNo).matches();
     }
-
     public static boolean isValidEmail(String email) {
         Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?");
         return pattern.matcher(email).matches();
     }
-
-    static boolean isValidPassword1(String password1) {
+    public static boolean isValidPassword1(String password1) {
         Pattern pattern = Pattern.compile("^[a-zA-z0-9]{8,}$");
         return pattern.matcher(password1).matches();
     }
-
-    static boolean isValidPassword2(String password2) {
+    public static boolean isValidPassword2(String password2) {
         Pattern pattern = Pattern.compile("^[A-Z][a-zA-z0-9]{8,}$");
         return pattern.matcher(password2).matches();
     }
-
+    public static boolean isValidPassword3(String password3) {
+        Pattern pattern = Pattern.compile("^[A-Z][a-zA-z1-9]{6,}[1-9]$");
+        return pattern.matcher(password3).matches();
+    }
 
     public static void main(String[] args) {
         System.out.println(isValidFirstName("Akash"));
@@ -43,6 +41,7 @@ public class UserRegistration {
         System.out.println(isValidPhoneNo("91 9423438559"));
         System.out.println(isValidEmail("akashpakhare499@gmail.com"));
         System.out.println(isValidPassword1("akash499"));
-        System.out.println(isValidPassword2("akash499"));
+        System.out.println(isValidPassword2("Akash499"));
+        System.out.println(isValidPassword3("Akashpakhare4"));
     }
 }
